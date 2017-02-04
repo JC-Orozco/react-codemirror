63c39795a87ac225f7f8cc90add79eea3ca951ff
Juan Carlos Orozco <jorozconews@gmail.com>
2017-02-04 08:51:54 -0600

    .

--- webpack.config.js
+++ webpack.config.js
@@ -5,40 +5,49 @@ var BUILD_DIR = path.resolve(__dirname, 'dist');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-  entry: 
-    APP_DIR + '/CodeMirror.jsx',
+  entry: {
+    app: APP_DIR + '/CodeMirror.jsx',
+    vendor: ["react", "react-dom"]
+  },
+    //APP_DIR + '/CodeMirror.jsx',
     //APP_DIR + '/index.jsx',
   output: {
     path: BUILD_DIR,
     filename: 'react-codemirror.js',
-    library: 'ACE',
-    libraryTarget: 'umd' // 'var' // 'umd'
+//    library: 'ACE',
+//    libraryTarget: 'umd' // 'var' // 'umd'
   },
   module : {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /(node_modules|bower_components)/,
-		externals: {
-			'react': {
-			  root: 'React',
-			  commonjs2: 'react',
-			  commonjs: 'react',
-			  amd: 'react'
-			},
-			'react-dom': {
-			  root: 'ReactDOM',
-			  commonjs2: 'react-dom',
-			  commonjs: 'react-dom',
-			  amd: 'react-dom'
-			}
-		  },
+//		externals: {
+//			'react': {
+//			  root: 'React',
+//			  commonjs2: 'react',
+//			  commonjs: 'react',
+//			  amd: 'react'
+//			},
+//			'react-dom': {
+//			  root: 'ReactDOM',
+//			  commonjs2: 'react-dom',
+//			  commonjs: 'react-dom',
+//			  amd: 'react-dom'
+//			}
+//		  },
         loader: 'babel-loader',
         query: {
           //presets: ['react', 'es2015', 'stage-0'],
           //plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
         }
       }
+    ],
+    // https://webpack.github.io/docs/code-splitting.html#split-app-and-vendor-code
+    plugins: [
+      //new webpack.optimize.DedupePlugin(),
+      //new webpack.optimize.OccurenceOrderPlugin(true),
+      new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */BUILD_DIR + "vendor.bundle.js")
     ]
 
 //    loaders : [
